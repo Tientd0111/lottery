79d4e5792ba62b7b04e43240e81bd757e6bf3d5c
phamdat1210 <62377341+phamdat1210@users.noreply.github.com>
2022-04-19 10:35:19 +0700

    change uri

--- src/apis/environmentConfig.js
+++ src/apis/environmentConfig.js
@@ -1,6 +1,6 @@
 let ConfigApi;
 // eslint-disable-next-line no-unused-vars
 export default ConfigApi = {
-	API_ENVIRONMENT_URL: 'https://1c81-2001-ee0-41c1-7ab3-89c4-a373-4e4b-f583.ap.ngrok.io/api/v1/',
+	API_ENVIRONMENT_URL: 'https://f4dc-2001-ee0-41c1-7ab3-829-1a9a-83d8-e66c.ap.ngrok.io/api/v1/',
 	TIME_OUT: 6000,
 }
--- src/stores/useSocket.js
+++ src/stores/useSocket.js
@@ -1,6 +1,6 @@
 import create from 'zustand';
 import { io } from "socket.io-client";
-const socket = io('https://1c81-2001-ee0-41c1-7ab3-89c4-a373-4e4b-f583.ap.ngrok.io', { transports: ["websocket", "polling"] });
+const socket = io('https://f4dc-2001-ee0-41c1-7ab3-829-1a9a-83d8-e66c.ap.ngrok.io', { transports: ["websocket", "polling"] });
 socket.connect()
 
 export const useSocket = create(set => ({
