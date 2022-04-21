3a927335f3722284dab56a0784e3673c2ab3470b
hqltech <hqltechcompany@gmail.com>
2022-04-21 15:37:30 +0700

    Update LiveChat.js

--- src/lode/components/LiveChat.js
+++ src/lode/components/LiveChat.js
@@ -1,38 +1,61 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import images from "../../assets/images/images";
+import {useSocket} from "../../stores/useSocket";
+import {callService} from "../../apis/baseRequest";
+import {useUserStore} from "../../stores/useUserStore";
+import {toast} from "react-toastify";
 
 const LiveChat = () => {
+
+	const [messages, setMessages] = useState([]);
+
+	const {socket} = useSocket(state => ({socket: state.socket}));
+	const {user} = useUserStore(state => ({user: state.user}));
+
+	const msgRef = useRef()
+
+	useEffect(()=>{
+		callService('messages', 'GET', {}).then((res)=>{setMessages(res)})
+		const addMessage = (msg) => setMessages(prevMessages => [...prevMessages, msg]);
+		socket.on('message', addMessage)
+		return () => {
+			socket.off('message', addMessage);
+		}
+	},[])
+
+	const sendMessage = async () => {
+		if(user?.username === undefined) {
+			toast('Vui lòng đăng nhập!')
+		} else {
+			const message = msgRef.current.value
+			if(message) {
+				await callService('messages/send', 'POST', {message: message}, true)
+				msgRef.current.value = ''
+			}
+		}
+	}
+
 	return (
 		<div className={"box-chat"} style={{marginTop:'100px'}}>
 			<div className={"chat-title"}>
 				Góc bàn đề
 			</div>
 			<div className={"chat-content"}>
-				<div className={"line-chat"}>
-					<span className={"user-name"}>user</span>
-					<span className={"chat"}>web lừa đảo anh em đừng chơi</span>
-				</div>
-				<div className={"line-chat"}>
-					<span className={"user-name"}>admin</span>
-					<span className={"chat"}>lừa cmm không chơi thì cút</span>
-				</div>
-				<div className={"line-chat"}>
-					<span className={"user-name"}>user2</span>
-					<span className={"chat"}>Bình tĩnh nào anh trai</span>
-				</div>
-				<div className={"line-chat"}>
-					<span className={"user-name"}>admin</span>
-					<span className={"chat"}>Chơi ngu thua kêu lừa đảo</span>
-				</div>
+				{messages.map((it)=>(
+					<div className={"line-chat"}>
+						<span className={"user-name"}>{it?.display_name}</span>
+						<span className={"chat"}>{it?.message}</span>
+					</div>
+				))}
 			</div>
 			<div className={"box-send"} style={{marginBottom:'20px'}}>
-				<input type="text" className={"txt-chat input-message"} style={{border:'none'}}/>
+				<input ref={msgRef} type="text" className={"txt-chat input-message"} style={{border:'none'}}/>
 				<div className={"icon-emoji"}>
 					<img src={images.icon_pick_emoji} alt=""/>
 				</div>
-				<button className={"btn-send send-message"}>
+				<div onClick={sendMessage} className={"btn-send send-message"}>
 					<img src={images.icon_send} alt=""/>
-				</button>
+				</div>
 			</div>
 
 		</div>
