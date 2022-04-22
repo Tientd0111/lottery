f559953df5926f6d94f3f5985a5849a0c0ddb0e4
Tientd0111 <76084002+Tientd0111@users.noreply.github.com>
2022-04-22 16:29:30 +0700

    commit

--- src/stores/useSocket.js
+++ src/stores/useSocket.js
@@ -1,6 +1,6 @@
 import create from 'zustand';
 import { io } from "socket.io-client";
-const socket = io('http://192.168.1.7:9080', { transports: ["websocket", "polling"] });
+const socket = io('https://winclubser.herokuapp.com', { transports: ["websocket", "polling"] });
 socket.connect()
 
 export const useSocket = create(set => ({
