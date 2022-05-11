4e0e9dbbb4dcc84f09e13f1054351483b2218509
Tientd0111 <76084002+Tientd0111@users.noreply.github.com>
2022-05-11 18:09:27 +0700

    update

--- src/hooks/formatNumber.js
+++ src/hooks/formatNumber.js
@@ -2,8 +2,5 @@ const numeral = require('numeral');
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (num) {
-	if(typeof num === 'number') {
-		return numeral(num*1000).format(' 0 a')
-	}
-	return ''
+	return num
 }
